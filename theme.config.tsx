44d329243099985ba570978e02761fcf1a7cbdb8
stefanus.suwandi <stefanus.suwandi@binus.edu>
2023-03-13 16:07:32 +0700

    added categories docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,7 +22,7 @@ const config: DocsThemeConfig = {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s – Votify'
+        titleTemplate: 'Votify'
       }
     }
   }
